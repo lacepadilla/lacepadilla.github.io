59472e551b73abbf71ecaa084f7b27fbba76f16e
Lace Padilla <lace.m.k.padilla@gmail.com>
2023-10-28 17:26:50 -0700

    php update

---
+++ footer.php
@@ -0,0 +1,9 @@
+<!-- #####Begin footer-->
+      <footer id="footer" class="dark-wrapper">
+		 <div class="copyright">© Copyright Lace Padilla <span id="year"></span></div>
+          
+		  <script>
+document.getElementById("year").innerHTML = new Date().getFullYear();
+</script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,104 @@
+
+<?php include 'header.php';?>
+
+    <div class="col-md-8 ml-10">
+     
+     <div class="intro">
+      <p> I am an Assistant Professor of Computer Science and Psychology at Northeastern University and a member of the <a class="black" href="https://vis.khoury.northeastern.edu/" title="Data Visualization Lab" target="new">Data Visualization Lab @Khoury</a>. My interests lie in the intersection between Information Visualization, Behavioral Decision Making, and Data Science. My research on uncertainty communication explores how to <strong><span class="highlight">align data visualizations of future events with human decision-making capabilities</span></strong>. I have received the <a class="black" href="http://space.ucmerced.edu/COVID_IEEEVIS2022" title="Multiple Forecast Visualizations (MFVs): Trade-offs in Trust and Performance in Multiple COVID-19 Forecast Visualizations" target="new">best paper award at IEEE VIS</a>, honorable mentions (<a class="black" href="https://vis.khoury.northeastern.edu/pubs/Moritz2023AverageEstimatesLine/">1</a>,<a class="black" href="https://hamza-elhamdadi.github.io/visposter2022.pdf">&nbsp;2</a>), and <a class="black" href="https://www.apadivisions.org/division-3/publications/bulletin/2021/06/awards">APA Early Career Award</a>, and an <a class="black" href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=2238175" title="NSF CAREER Award Link" target="new">NSF CAREER Award</a>. I am the PI and Co-PI on several other grants funded by NSF (#<a class="black" href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=2122174" title="Facilitating Restoration of Natural Infrastructure Using Uncertainty Communication" target="new">2122174</a>, #<a class="black" href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=2028374&HistoricalAwards=false" title="Visualizing Epidemical Uncertainty for Personal Risk Assessment" target="new">2028374</a>, #<a class="black" href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1810498&HistoricalAwards=false" title="Improving Graph Literacy and Numeracy" target="new">1810498</a>) and DOE.<br/><br/>
+
+ In my spare time, I am a strong advocate for minoritized groups in STEM, previously serving on the <a class="black" href="http://ieeevis.org/year/2022/info/committees/conference-committee" title="IEEE VIS Inclusivity Committee Link" target="new">IEEE VIS Inclusivity Committee</a> and the Governing Board of <a class="black" href="https://www.sparksociety.org/" title="Spark Society Link" target="new">Spark Society</a>. I have received several grants and awards for my diversity work. </p> 
+		   </div>
+      
+      <h2>Selected Publications</h2>
+		
+		<div class="eachpub">
+          <div class="meta">
+             
+              <div class="title"><i class="fa fa-star-o" aria-hidden="true"></i> <span class="conference">VIS 2023 (Best Paper Award Honerable Mention):</span> Average Estimates in Line Graphs are Biased Toward Areas of Higher Variability. </div>
+              <div class="authors">Moritz, D., <span class="bold">Padilla, L.</span>, Nguyen, F., Castro, S.,  Bertini, E. </div> 
+			   <a target="_blank" href="https://www.domoritz.de/papers/2023-LineBias-VIS.pdf"> PDF </a> |  <a target="_blank" href="https://osf.io/aupbk/"> Data + Analysis</a> | <a target="_blank" href="https://osf.io/aupbk/registrations"> Pre-registration </a> | <a href="https://www.slideshare.net/domoritz/2023-vis-line-bias">Slides </a>
+          </div>
+        </div>
+		
+		
+  
+		<div class="eachpub">
+          <div class="meta">
+             
+              <div class="title"> <i class="fa fa-trophy" aria-hidden="true"></i> <span class="conference"> VIS 2022 (Best Paper Award):</span> Multiple Forecast Visualizations (MFVs): Trade-offs in Trust and Performance in Multiple COVID-19 Forecast Visualizations. </div>
+              <div class="authors"><span class="bold">Padilla, L.</span>, Fygenson, R., Franconeri, S. L., </div> 			  
+			  <a target="_blank" href="https://psyarxiv.com/2sq7j"> PDF </a> |  <a target="_blank" href="https://osf.io/pcmvq/"> Data + Analysis</a> | <a target="_blank" href="https://osf.io/e2fnd"> Pre-registration</a>
+	      </div>
+        </div>
+		
+		<div class="eachpub">
+          <div class="meta">
+             
+              <div class="title"> Impact of COVID-19 forecast visualizations on pandemic risk perceptions. </div>
+			  <div class="authors"><span class="bold">Padilla, L.</span>, Hosseinpour, H., Fygenson, R., Howell, J., Chunara, R., and Bertini, E. </div> Nature's Scientific Reports, 2022.<br/>
+			      
+          
+			<a target="_blank" href="https://www.nature.com/articles/s41598-022-05353-1"> PDF </a> |  <a target="_blank" href="https://osf.io/hfvqc/"> Data + Analysis </a>
+			</div>
+		   </div>
+		
+
+			
+		<div class="eachpub">
+          <div class="meta">
+             
+              <div class="title"> The Science of Visual Data Communication: What Works. </div>
+              <div class="authors">Franconeri, S. L., <span class="bold">Padilla, L.</span>, Shah, P., Zacks, J. M., and Hullman, J.</div> 
+			  Psychological Science in the Public Interest, 2021. <br/>
+			  <a target="_blank" href="https://journals.sagepub.com/doi/full/10.1177/15291006211051956"> PDF </a> 
+         </div>
+        </div>
+         
+
+
+      
+        <div class="eachpub">
+          <div class="meta">
+             
+              <div class="title"> Uncertainty Visualization (book chapter) </div>
+              <div class="authors"><span class="bold">Padilla, L.</span>., Kay, M. and Hullman, J. </div> Computational Statistics in Data Science, 2021<br/>
+              <a target="_blank" href="https://doi.org/10.1002/9781118445112.stat08296"> PDF </a>	
+          .</div>
+        </div>
+        
+      
+
+
+        <div class="eachpub">
+          <div class="meta">
+             
+              <div class="title"> <i class="fa fa-trophy" aria-hidden="true"></i> <span class="conference">APA Early Career Award:</span> The Powerful Influence of Marks: Visual and Knowledge-Driven Processing in Hurricane Track Displays. </div>
+              <div class="authors"><span class="bold">Padilla, L.</span>., Creem-Regehr, S. H., & Thompson, W. </div> Journal of Experimental Psychology: Applied, 2020. <br/>
+			  <a target="_blank" href="http://space.ucmerced.edu/Downloads/publications/Padillaetal_2019.pdf"> PDF </a>|  <a target="_blank" href="https://osf.io/j34g5/"> Data + Analysis</a> 
+			  
+          </div>
+        </div>
+        
+    
+        
+      
+      
+           
+        <div class="eachpub">
+          <div class="meta">
+             
+              <div class="title"> Decision Making with Visualizations: A Cognitive Framework Across Disciplines </div>
+              <div class="authors"><span class="bold">Padilla, L.</span>, Creem-Regehr, S. H., Hegarty, M., & Stefanucci, J. K. </div> Cognitive Research: Principles and Implications, 2018. <br/><a target="_blank" href="http://space.ucmerced.edu/Downloads/publications/Padillaetal2018-Vis-Decision_Review.pdf"> PDF </a>
+          
+          </div>
+        </div>                  
+
+
+        </div>
+      
+    </div>
+  </div>
+</div>
+	</div>
+  </body>
+<?php include 'footer.php';?>
