889b50fa2fd72ae46f26095cbdff933c185d9339
Lace Padilla <lace.m.k.padilla@gmail.com>
2023-10-28 17:36:51 -0700

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-<!-- #####Begin footer-->
-      <footer id="footer" class="dark-wrapper">
-		 <div class="copyright">© Copyright Lace Padilla <span id="year"></span></div>
-          
-		  <script>
-document.getElementById("year").innerHTML = new Date().getFullYear();
-</script>
-  </body>
-</html>
